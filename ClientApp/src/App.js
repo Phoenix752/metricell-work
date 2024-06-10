031d8a8d271a67e58864dd46a90696f79fb2e8fb
BuildTools <unconfigured@null.spigotmc.org>
2024-06-10 16:56:41 +0100

    added list

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -7,6 +7,8 @@ export default function () {
     }
 
     async function createEmployee(name, value) {
+        setInputName("");
+        setInputValue("");
         return fetch("/employees", {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -15,6 +17,8 @@ export default function () {
     }
 
     async function updateEmployee(name, value) {
+        setInputName("");
+        setInputValue("");
         return fetch("/employees", {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
@@ -22,7 +26,45 @@ export default function () {
         });
     }
 
+    const [employeeData, setEmployeeData] = useState([]);
+    const [inputName, setInputName] = useState('');
+    const [inputValue, setInputValue] = useState('');
+
+    const handleNameInput = (event) => {
+        setInputName(event.target.value);
+    };
+    const handleValueInput = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getEmployees();
+                console.log(data);
+                setEmployeeData(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <div>Complete your app here</div>
+        <div>
+        <h1>Employees</h1>
+        <input type="text" value={inputName} onChange={handleNameInput} placeholder="Enter employee name"/>
+        <input type="text" value={inputValue} onChange={handleValueInput} placeholder="Enter employee value"/>
+        <button onClick={() => createEmployee(inputName,inputValue)}>Add new employee</button>
+        <button onClick={() => updateEmployee(inputName,inputValue)}>Update value of an employee</button>
+        <ul>
+            {employeeData.map((employee, index) => (
+                <li key={index}>
+                     {employee.name} - {employee.value}
+                </li>
+            ))}
+        </ul>
+    </div>
     );
 }
